26981afab26539890d498d85dc277cb7487a2219
Lautar0 <104235333+Saturno0@users.noreply.github.com>
2025-10-30 15:56:30 -0300

    Actualize la ruta del navbar para poder crear un nuevo producto

--- frontend/src/components/LeftMenu.tsx
+++ frontend/src/components/LeftMenu.tsx
@@ -1,44 +1,63 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import type { UserState } from "../types/types";
 
 interface LeftMenuProps {
-     menuOpen: boolean,
-     setMenuOpen: (open: boolean) => void,
-     handleNavigation: () => void,
-     user: UserState
+  menuOpen: boolean;
+  setMenuOpen: (open: boolean) => void;
+  handleNavigation: () => void;
+  user: UserState;
 }
 
-const LeftMenu: React.FC<LeftMenuProps> = ({ menuOpen, setMenuOpen, handleNavigation, user }) => {
-    return (
-        <>
-        <div className="menu-icon" id="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
-            ☰
-        </div>
-        <div className="left-menu">
-            <ul className={`nav-links ${menuOpen ? 'open' : ''}`} id="lista">
-                <li className="links">
-                    <Link to={{
-                        pathname: "/",
-                        hash: "#products"
-                    }}>
-                        <h3>Productos</h3>
-                    </Link>
-                </li>
-                <li className="links">
-                    <Link to='/newIn'>
-                        <h3>Nuevo ingreso</h3>
-                    </Link>
-                </li>
-                <li className="links"><h3>Mas vendidos</h3></li>
-                {menuOpen && (
-                    <li onClick={() => handleNavigation()}>
-                    <h3>{user.activo? `${user.nombre}` : "Acceder"}</h3>
-                    </li>
-                )}
-            </ul>
-        </div>
-        </>
-    );
-}
+const LeftMenu: React.FC<LeftMenuProps> = ({
+  menuOpen,
+  setMenuOpen,
+  handleNavigation,
+  user,
+}) => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div
+        className="menu-icon"
+        id="menu-icon"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
+      </div>
+      <div className="left-menu">
+        <ul className={`nav-links ${menuOpen ? "open" : ""}`} id="lista">
+          <li className="links">
+            <Link
+              to={{
+                pathname: "/",
+                hash: "#products",
+              }}
+            >
+              <h3>Productos</h3>
+            </Link>
+          </li>
+          <li className="links">
+            <Link to="/newIn">
+              <h3>Nuevo ingreso</h3>
+            </Link>
+          </li>
+          <li className="links">
+            <h3>Mas vendidos</h3>
+          </li>
+          {menuOpen && (
+            <>
+              <li onClick={() => navigate("/create-product")}>
+                <h3>Crear producto</h3>
+              </li>
+              <li onClick={() => handleNavigation()}>
+                <h3>{user.activo ? `${user.nombre}` : "Acceder"}</h3>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+    </>
+  );
+};
 
 export default LeftMenu;
